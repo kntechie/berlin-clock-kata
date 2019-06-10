30677a292169a40f8e354fdcc1966c656c8a40aa
2019-SOW <kumaran26kumaran@gmail.com>
2019-06-10 12:16:08 +0200

    Refactor - Moved all constants to a separate file

--- src/components/Minutes/Minutes.js
+++ src/components/Minutes/Minutes.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import constants from '../../utils/constants';
 
 class Minutes extends React.Component {
 
     bottomMinutes() {
-        return this.getOnOffLamps(4);
+        return this.getOnOffLamps(constants.TOTAL_BOTTOM_MINUTES_LAMP);
     }
 
     getOnOffLamps = (totalLamps) => {
         let row = "";
 
         for (let i = 0; i < totalLamps; i++) {
-          row += "O";
+          row += constants.LAMP_OFF;
         }
         
         return row;
---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+const constants = {
+    LAMP_OFF: 'O',
+    TOTAL_BOTTOM_MINUTES_LAMP: 4
+}
+
+export default constants;
